96496c3154f6a4b20abdd4887ae164a3b1fef064
Nightmare-MY <906262255@qq.com>
2020-12-12 12:10:02 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/nightmare/termare/termare_pty/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nightmare.termare.termare_pty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/nightmare/termarepty/termare_pty/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nightmare.termarepty.termare_pty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
